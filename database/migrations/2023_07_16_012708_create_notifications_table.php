607d75b1a0e78af656f9afaf246e9215b2d47d37
ATSU <atsgula@gmail.com>
2023-07-15 22:37:17 -0500

    temporal

--- database/migrations/2023_07_16_012708_create_notifications_table.php
+++ database/migrations/2023_07_16_012708_create_notifications_table.php
@@ -11,18 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Cambiar el motor de almacenamiento temporalmente a MyISAM
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->engine = 'MyISAM';
+        });
+
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('type', 191);
+            $table->string('type', 100); // Reducir la longitud de la columna 'type' a VARCHAR(100)
             $table->morphs('notifiable');
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
+
+            // Agregar un índice prefix para notifiable_type y notifiable_id
+            $table->index(['notifiable_type', 'notifiable_id'], 'notifications_notifiable_index', 'btree', 190)->collation('utf8mb4_bin');
         });
 
-        // Add a prefix index for notifiable_type and notifiable_id
+        // Cambiar de vuelta a InnoDB
         Schema::table('notifications', function (Blueprint $table) {
-            $table->index(['notifiable_type', 'notifiable_id'], 'notifications_notifiable_index')->collation('utf8mb4_bin');
+            $table->engine = 'InnoDB';
         });
     }
 
