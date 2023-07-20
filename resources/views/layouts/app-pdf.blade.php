b887d8372905abd946a60977047843a2251aef1c
ATSU <atsgula@gmail.com>
2023-07-20 15:47:30 -0500

    cambio derechos autor

--- resources/views/layouts/app-pdf.blade.php
+++ resources/views/layouts/app-pdf.blade.php
@@ -22,7 +22,7 @@
             <div class="pull-right hidden-xs">
                 <b>Version</b> 1.0
             </div>
-            <strong>Copyright©2023.</strong> All rights reserved. <strong>ATS</strong>
+            <strong>Copyright©2023.</strong> All rights reserved. <strong>ATSU</strong>
         </footer>
         {{-- Custom Scripts --}}
         @yield('js')
