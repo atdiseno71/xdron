22f609f81aecca36c8855e2e9e202817c3be32ed
ATSU <atsgula@gmail.com>
2023-08-02 08:25:00 -0500

    Producto final

--- resources/views/operacion/form.blade.php
+++ resources/views/operacion/form.blade.php
@@ -110,7 +110,18 @@
     </div>
     @include('layouts.btn-submit')
 
-    <script src="{{ asset('js/plugins/image.preview.js') }}"></script>
-    <script src="{{ asset('js/plugins/sweetalert.js') }}"></script>
+    <script>
+        function previewFile(event) {
+            var input = event.target;
+            var reader = new FileReader();
+            reader.onload = function(){
+                var dataURL = reader.result;
+                var previewId = $(input).data('preview-id');
+                var img = document.getElementById(previewId);
+                img.src = dataURL;
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    </script>
 
 </div>
