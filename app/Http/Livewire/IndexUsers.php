61d4c0f94d047edfb30d609e7546d544cff4a660
-_- <atsgula@gmail.com>
2023-09-22 20:59:54 -0500

    fix list super-root

--- app/Http/Livewire/IndexUsers.php
+++ app/Http/Livewire/IndexUsers.php
@@ -16,16 +16,19 @@ class IndexUsers extends Component
 
     protected $paginationTheme = 'bootstrap';
 
-    public function updatingSearch() {
+    public function updatingSearch()
+    {
         $this->resetPage();
     }
 
     public function render()
     {
-        $users = User::where('name', 'LIKE', '%' . $this->search . '%')
-            ->OrWhere('lastname', 'LIKE', '%' . $this->search . '%')
-            ->OrWhere('email', 'LIKE', '%' . $this->search . '%')
-            ->paginate();
+        $users = User::whereNotIn('id_role', [config('roles.super_root')])
+            ->where(function ($query) {
+                $query->orWhere('name', 'LIKE', '%' . $this->search . '%')
+                    ->orWhere('lastname', 'LIKE', '%' . $this->search . '%')
+                    ->orWhere('email', 'LIKE', '%' . $this->search . '%');
+            })->with('roles')->paginate();
 
         return view('livewire.index-users', compact('users'));
     }
