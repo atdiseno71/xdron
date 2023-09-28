114d409eee07a8ec1a207cf1d2854f54a38b16b9
-_- <atsgula@gmail.com>
2023-09-28 17:04:52 -0500

    fix demo

--- app/Http/Controllers/V2/OperationController.php
+++ app/Http/Controllers/V2/OperationController.php
@@ -288,6 +288,7 @@ class OperationController extends Controller
                 if (in_array($fieldName, $file_name) && $request->has($fieldName)) {
                     $handle_1 = $this->webpImage($request, $fieldName, $folder, $fieldName);
                     $detail_temp[$input] = $handle_1;
+                    dd($handle_1);
                 }
                 // Agregar el valor al arreglo de datos
                 else if ($request->has($fieldName)) {
