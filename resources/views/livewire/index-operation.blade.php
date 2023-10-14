132c34edc13cc8ddc2327b910ec74de956c4dcaf
-_- <atsgula@gmail.com>
2023-10-13 19:57:59 -0500

    fix format date creation

--- resources/views/livewire/index-operation.blade.php
+++ resources/views/livewire/index-operation.blade.php
@@ -136,8 +136,8 @@
                                         <td>{{ $operation->userPilot?->name }}</td>
                                         <td>{{ $operation->client?->social_reason }}</td>
                                         <td>{{ $operation->userAdmin?->name }}</td>
-                                        <td>{{ $operation->created_at }}</td>
-                                        <td>{{ $operation->details[0]?->created_at }}</td>
+                                        <td>{{ $operation->created_at?->format('d/m/Y') }}</td>
+                                        <td>{{ $operation->details[0]?->created_at?->format('d/m/Y') }}</td>
                                         <td>{{ $operation->status?->name ?? 'Sin vuelos.' }}</td>
 
                                     </tr>
