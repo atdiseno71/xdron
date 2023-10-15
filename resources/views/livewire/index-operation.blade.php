971ffb1fc174eb29b69d13ad10c3aaee7cd110ac
-_- <atsgula@gmail.com>
2023-10-14 20:56:09 -0500

    fix array key 0

--- resources/views/livewire/index-operation.blade.php
+++ resources/views/livewire/index-operation.blade.php
@@ -147,7 +147,7 @@
                                         <td>{{ $operation->client?->social_reason }}</td>
                                         <td>{{ $operation->userAdmin?->name }}</td>
                                         <td>{{ $operation->created_at?->format('d/m/Y') }}</td>
-                                        <td>{{ $operation->details[0] == null ? 'Sin vuelos.' : $operation->details[0]->created_at?->format('d/m/Y') }}</td>
+                                        <td>{{ count($operation->details) == 0 ? 'Sin vuelos.' : $operation->details[0]->created_at?->format('d/m/Y') }}</td>
                                         <td>{{ $operation->status?->name ?? 'Sin vuelos.' }}</td>
 
                                     </tr>
